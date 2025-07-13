ae8b2fd1c0a138c7a2134baa0bc949954d14ad8a
eomsarang <rosy.sarang.eom@gmail.com>
2025-07-13 12:25:17 +0900

    .

--- hello.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-// 프로그램 진입점(entry point)
-int main(void)
-{
-  printf("Hello\n");
-  return 0;
-}
--- java-lang/src/main/java/Hello2.java
+++
@@ -1,5 +0,0 @@
-public class Hello2 {
-  public static void main2(String[] args) {
-    System.out.println("Hello");
-  }
-}
