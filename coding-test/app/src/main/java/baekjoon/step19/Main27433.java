467845da9a7d507d80b16a6aae848ef85aeda8f7
eomsarang <rosy.sarang.eom@gmail.com>
2025-09-20 21:55:01 +0900

    코딩테스트- 재귀(1)

---
+++ coding-test/app/src/main/java/baekjoon/step19/Main27433.java
@@ -0,0 +1,16 @@
+package baekjoon.step19;
+
+import java.util.Scanner;
+
+public class Main27433 {
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int N = sc.nextInt();
+    long result = 1;
+    for (int i = 2; i <= N; i++) {
+      result *= i;
+    }
+    System.out.println(result);
+  }
+}
