7b5a347cd491009bbabd6bfd241a326b878c819a
eomsarang <rosy.sarang.eom@gmail.com>
2025-09-21 21:06:23 +0900

    코딩테스트 재귀(2)

---
+++ coding-test/app/src/main/java/baekjoon/step19/Main25501.java
@@ -0,0 +1,37 @@
+package baekjoon.step19;
+
+import java.util.Scanner;
+
+public class Main25501 {
+
+  static int count;
+
+  public static int recursion(String s, int l, int r) {
+    count++;
+    if (l >= r) {
+      return 1;
+    } else if (s.charAt(l) != s.charAt(r)) {
+      return 0;
+    } else {
+      return recursion(s, l + 1, r - 1);
+    }
+  }
+
+  public static int isPalindrome(String s) {
+    return recursion(s, 0, s.length() - 1);
+  }
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int T = sc.nextInt();
+    sc.nextLine();
+
+    for (int i = 0; i < T; i++) {
+      String s = sc.nextLine();
+      count = 0;
+      int result = isPalindrome(s);
+      System.out.println(result + " " + count);
+    }
+  }
+}
+
