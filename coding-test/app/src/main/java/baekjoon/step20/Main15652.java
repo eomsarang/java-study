e1406449469665379cf60dd370aa1364831b1a85
eomsarang <rosy.sarang.eom@gmail.com>
2025-09-29 23:37:39 +0900

    코딩테스트- 백트래킹(4)

---
+++ coding-test/app/src/main/java/baekjoon/step20/Main15652.java
@@ -0,0 +1,41 @@
+package baekjoon.step20;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main15652 {
+
+  static int N, M;
+  static int[] arr;
+  static StringBuilder sb = new StringBuilder();
+
+  public static void dfs(int depth, int start) {
+    if (depth == M) {
+      for (int i = 0; i < M; i++) {
+        sb.append(arr[i]).append(" ");
+      }
+      sb.append("\n");
+      return;
+    }
+
+    for (int i = start; i <= N; i++) {
+      arr[depth] = i;
+      dfs(depth + 1, i);
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+
+    N = Integer.parseInt(st.nextToken());
+    M = Integer.parseInt(st.nextToken());
+
+    arr = new int[M];
+
+    dfs(0, 1);
+    System.out.print(sb);
+  }
+}
