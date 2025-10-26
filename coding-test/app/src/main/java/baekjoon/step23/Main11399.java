e81404c2b8d34600aa70c582a95f031aa2638b7b
eomsarang <rosy.sarang.eom@gmail.com>
2025-10-27 03:25:36 +0900

    코딩테스트- 그리디 알고리즘1(3)

---
+++ coding-test/app/src/main/java/baekjoon/step23/Main11399.java
@@ -0,0 +1,34 @@
+package baekjoon.step23;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main11399 {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    int N = Integer.parseInt(br.readLine());
+    int[] P = new int[N];
+
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    for (int i = 0; i < N; i++) {
+      P[i] = Integer.parseInt(st.nextToken());
+    }
+
+    Arrays.sort(P);
+
+    int total = 0;
+    int sum = 0;
+
+    for (int i = 0; i < N; i++) {
+      sum += P[i];
+      total += sum;
+    }
+
+    System.out.println(total);
+  }
+}
