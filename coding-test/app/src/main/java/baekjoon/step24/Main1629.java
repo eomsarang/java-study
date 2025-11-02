8b74bb0bb0ba300ac3f4c0cd54d987fc1355e3a8
eomsarang <rosy.sarang.eom@gmail.com>
2025-11-02 14:53:09 +0900

    코딩테스트- 분할 정복(4)

---
+++ coding-test/app/src/main/java/baekjoon/step24/Main1629.java
@@ -0,0 +1,36 @@
+package baekjoon.step24;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main1629 {
+
+  static long A, B, C;
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+
+    A = Long.parseLong(st.nextToken());
+    B = Long.parseLong(st.nextToken());
+    C = Long.parseLong(st.nextToken());
+
+    System.out.println(pow(A, B));
+  }
+
+  static long pow(long a, long b) {
+    if (b == 1) {
+      return a % C;
+    }
+
+    long temp = pow(a, b / 2);
+
+    if (b % 2 == 0) {
+      return (temp * temp) % C;
+    } else {
+      return ((temp * temp) % C * a) % C;
+    }
+  }
+}
