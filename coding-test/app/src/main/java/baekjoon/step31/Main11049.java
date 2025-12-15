8636102979b5676b07d95bb4ca42eab18befad5a
eomsarang <rosy.sarang.eom@gmail.com>
2025-12-16 00:21:22 +0900

    코딩테스트- 동적 계획법2(2)

---
+++ coding-test/app/src/main/java/baekjoon/step31/Main11049.java
@@ -0,0 +1,42 @@
+package baekjoon.step31;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main11049 {
+
+  static final long INF = Long.MAX_VALUE / 2;
+
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int N = Integer.parseInt(br.readLine());
+
+    int[] r = new int[N + 1];
+    int[] c = new int[N + 1];
+
+    for (int i = 1; i <= N; i++) {
+      StringTokenizer st = new StringTokenizer(br.readLine());
+      r[i] = Integer.parseInt(st.nextToken());
+      c[i] = Integer.parseInt(st.nextToken());
+    }
+
+    long[][] dp = new long[N + 1][N + 1];
+
+    for (int len = 2; len <= N; len++) {
+      for (int i = 1; i + len - 1 <= N; i++) {
+        int j = i + len - 1;
+        dp[i][j] = INF;
+
+        for (int k = i; k < j; k++) {
+          long cost = dp[i][k] + dp[k + 1][j]
+              + (long) r[i] * c[k] * c[j];
+          dp[i][j] = Math.min(dp[i][j], cost);
+        }
+      }
+    }
+
+    System.out.println(dp[1][N]);
+  }
+}
+
